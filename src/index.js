d07ce3e24d27731e75484132f85e2acfe83b1af4
Ingrid Su(蘇毓瑄) <ingridsu@gamania.com>
2021-10-19 09:58:02 +0800

    Add bootstrap, sass-loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render( <div>hello React!</div> , document.getElementById('root') );
+import App from './App';
+import './App.scss';
+
+ReactDOM.render( <App /> , document.getElementById('root') );
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
     mode: 'development',
     entry: {
@@ -20,8 +22,13 @@ module.exports = {
                     }
                 }
             },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
         ]
     },
+    plugins: [new MiniCssExtractPlugin()],
     devServer: {
         static: {
             directory: path.join(__dirname, 'build'),
