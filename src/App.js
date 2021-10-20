2e3772ecaf9dea9cd2a9a563963bcff536463557
Ingrid Su(蘇毓瑄) <ingridsu@gamania.com>
2021-10-20 15:07:18 +0800

    Create ListButton component, _colors.scss

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import './App.scss';
 import Wrapper from './UI/Wrapper'
 import { Row, Col } from 'react-bootstrap';
+
+import ListButton from './UI/ListButton'
 
 const App = props => {
     return (
         <>
             <Wrapper>
                 <Row>
-                    <Col sm={2}>sm=2</Col>
-                    <Col sm={10}>sm=10</Col>
+                    <Col sm={2} className="align-center">
+                        <ListButton className="curser-pointer" name="Messages"/>
+                    </Col>
+                    <Col sm={10} className="align-center">sm=10</Col>
                 </Row>
             </Wrapper>
         </>
