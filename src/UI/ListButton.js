3203ed9e00896a4c13378a94db168393b519d4a2
Ingrid Su <isingridsu@gmail.com>
2021-10-22 14:28:57 +0800

    install react-icons, add Messages icon

--- src/UI/ListButton.js
+++ src/UI/ListButton.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './ListButton.scss';
+import { FaRegEnvelope } from "react-icons/fa";
 
 const ListButton = props => {
     return (
         <>
             <div className={`${props.className} list-button`}>
-                <div >{props.name}</div>
+                <FaRegEnvelope style={{ fontSize: '30px'}} />
+                <div className="list-button__name">{props.name}</div>
             </div>
         </>
     )
